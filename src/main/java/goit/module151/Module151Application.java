2ab28e9a04798142b3593bea0126bf8f93d2825a
MaksimHrachov <grmavi@gmail.com>
2023-09-25 09:39:58 +0300

    first commit

---
+++ src/main/java/goit/module151/Module151Application.java
@@ -0,0 +1,13 @@
+package goit.module151;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Module151Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Module151Application.class, args);
+    }
+
+}
---
+++ src/test/java/goit/module151/Module151ApplicationTests.java
@@ -0,0 +1,13 @@
+package goit.module151;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Module151ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
